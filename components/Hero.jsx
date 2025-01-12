82aaa78e5438dd08027fe0e6f6d8033863459931
Othman Ahmed <othmanahmed258@gmail.com>
2025-01-12 20:43:53 +0200

    adding the logo to the nav bar

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,16 +4,26 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section className="h-screen w-full bg-cover bg-no-repeat bg-[#181818] flex flex-col items-center justify-center">
+    <section className="relative overflow-hidden h-screen w-full bg-cover bg-no-repeat bg-[#181818] flex flex-col items-center justify-center">
       <Image
-        src="/img/hero-1.jpg"
+        src="/img/logo.png"
         width={800}
         height={800}
         alt="Eta logo"
-        className="translate-y-[-15%]"
+        className="translate-y-[-14%] z-30 absolute w-[50rem]"
       />
 
-      <Button name={"Contact Us"} />
+      <div className="translate-y-[380%] z-30">
+        <Button name={"Contact Us"} />
+      </div>
+
+      <Image
+        src="/img/subtle-prism.svg"
+        width={2000}
+        height={1000}
+        alt="topography background pattern"
+        className="w-[107rem] bg-cover absolute top-0 left-0 opacity-15"
+      />
     </section>
   );
 };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 const Nav = () => {
-  return <nav></nav>;
+  return (
+    <nav className="fixed flex justify-between items-center max-w-[1280px] z-50 px-[1rem] py-[1rem]">
+      <a href="#">
+        <Image
+          src="/img/logo.png"
+          height={1000}
+          width={1000}
+          alt="Eta logo"
+          className="w-[7rem]"
+        />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
