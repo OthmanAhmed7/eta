2cb763e083da7bd6488cd7a46514efebbcb937c6
Othman Ahmed <othmanahmed258@gmail.com>
2025-01-12 21:07:41 +0200

    adding menu button to the navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,19 +1,25 @@
 import Image from "next/image";
 import React from "react";
 
+import { RiMenuFoldFill } from "react-icons/ri";
+
 const Nav = () => {
   return (
-    <nav className="fixed flex justify-between items-center max-w-[1280px] z-50 px-[1rem] py-[1rem]">
-      <a href="#">
-        <Image
-          src="/img/logo.png"
-          height={1000}
-          width={1000}
-          alt="Eta logo"
-          className="w-[7rem]"
-        />
-      </a>
-    </nav>
+    <section className="fixed flex mx-auto w-[100%] h-[5rem] z-50 translate-x-[6%]">
+      <nav className="flex justify-between items-center max-w-[1440px] w-full px-[1rem] py-[1rem]">
+        <a href="#">
+          <Image
+            src="/img/logo.png"
+            height={1000}
+            width={1000}
+            alt="Eta logo"
+            className="w-[7rem] h-[3rem]"
+          />
+        </a>
+
+        <RiMenuFoldFill className="w-[2.5rem] h-[2.5rem] text-white" />
+      </nav>
+    </section>
   );
 };
 
