5ba5c07a925a01a1daae35c48598c195734923a9
Othman Ahmed <othmanahmed258@gmail.com>
2025-02-12 20:29:57 +0200

    creating the contact us section

---
+++ components/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <section className="mx-auto my-[8rem] max-w-[1440px] flex justify-center items-center gap-[4rem] px-[1rem]">
+      <div></div>
+
+      <div></div>
+    </section>
+  );
+};
+
+export default ContactUs;
