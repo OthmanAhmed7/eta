542a02e53d9085ef52e11ac0ff63b79529efc15f
Othman Ahmed <othmanahmed258@gmail.com>
2025-02-16 00:16:30 +0200

    responsiveness for articles section

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       <AboutUs />
       <LastProjects />
       <Services />
-      {/* <Articles /> */}
+      <Articles />
       {/* <ContactUs /> */}
       {/* <Footer /> */}
     </div>
--- components/Articles.jsx
+++ components/Articles.jsx
@@ -38,7 +38,7 @@ const Articles = () => {
         </h2>
       </div>
 
-      <div className="grid grid-cols-3 gap-[2rem]">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
         {articles.map((i) =>
           i.id <= articlesNumber ? (
             <div
