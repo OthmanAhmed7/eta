489b108d8a209175515b120b158b87e353eac351
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-01 10:34:22 +0200

    craete: pageHeader component

--- components/PageHeader.jsx
+++ components/PageHeader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Image from "next/image";
 
-const page = () => {
+const PageHeader = ({ pageName, header }) => {
   return (
     <>
       <section className="relative h-screen w-full bg-cover overflow-hidden bg-no-repeat bg-[#181818] flex flex-col items-center justify-center px-[1rem]">
@@ -12,9 +11,17 @@ const page = () => {
           alt="topography background pattern"
           className="bg-cover absolute top-0 left-0 opacity-15 h-[100vh]"
         />
+
+        <p className="text-white font-thin tracking-[.15em] text-[1.25rem]">
+          {pageName}
+        </p>
+
+        <h1 className="text-white text-[3rem] text-center font-[600]">
+          {header}
+        </h1>
       </section>
     </>
   );
 };
 
-export default page;
+export default PageHeader;
