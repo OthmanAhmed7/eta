4d34914a768ad1c1bca520b3c217838757ba3812
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-01 11:32:02 +0200

    edit: replace normal header with PageHeader component in contact us page

--- app/ContactUs/page.js
+++ app/ContactUs/page.js
@@ -1,20 +1,12 @@
 import React from "react";
-import Image from "next/image";
+import PageHeader from "@/components/PageHeader";
 import ContactUs from "@/components/ContactUs";
 import SectionHeader from "@/components/SectionHeader";
 
 const page = () => {
   return (
     <>
-      <section className="relative h-screen w-full bg-cover overflow-hidden bg-no-repeat bg-[#181818] flex flex-col items-center justify-center px-[1rem]">
-        <Image
-          src="/img/subtle-prism.svg"
-          width={2000}
-          height={1000}
-          alt="topography background pattern"
-          className="bg-cover absolute top-0 left-0 opacity-15 h-[100vh]"
-        />
-      </section>
+      <PageHeader pageName={"Contact Us"} header={"Let Us Connect With You."} />
 
       <ContactUs />
 
