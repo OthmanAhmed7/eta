1b2b871255a44a46f9c3a8748d7516617a92ece3
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-02 10:20:47 +0200

    craete: the page and section headers

--- app/Services/page.js
+++ app/Services/page.js
@@ -1,7 +1,20 @@
+import PageHeader from "@/components/PageHeader";
+import SectionHeader from "@/components/SectionHeader";
 import React from "react";
 
 const page = () => {
-  return <>Services</>;
+  return (
+    <>
+      <PageHeader
+        pageName={"Services"}
+        header={"Explore What We Can Do For You"}
+      />
+
+      <section className="flex flex-col m-auto items-start max-w-[1200px]">
+        <SectionHeader header={"Services"} title={"Our Way of Serving You"} />
+      </section>
+    </>
+  );
 };
 
 export default page;
