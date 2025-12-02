70253c6282de9dbd47fff3c4e579a25b11d7ca56
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-02 11:15:23 +0200

    create: services page sections

--- app/Services/page.js
+++ app/Services/page.js
@@ -14,11 +14,49 @@ const page = () => {
         <SectionHeader header={"Services"} title={"Our Way of Serving You"} />
 
         <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
-          Every project we craft carries its own signature identity. Eta Group
-          has enhanced hundreds of spaces across residential, commercial,
-          hospitality, and retail sectors, consistently delivering distinguished
-          results. Guided by our core values and an unwavering commitment to
-          quality, we continue to set new standards in unique interior design.
+          Every project we create carries a unique identity. Eta Group has
+          transformed hundreds of residential, commercial, hospitality, and
+          retail spaces, consistently delivering exceptional results. Guided by
+          our core values and commitment to quality, we continue to set new
+          standards in distinctive interior design.
+        </p>
+
+        <h3 className="text-slate-500 text-[1.2rem] font-[600]">Plan</h3>
+        <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+          At Eta Group, we deliver high-standard, tailored master planning with
+          punctuality, professionalism, and precision. For us, it’s a refined
+          blend of art and science—where creative vision meets real-world
+          practicality.
+        </p>
+
+        <h3 className="text-slate-500 text-[1.2rem] font-[600]">Design</h3>
+        <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+          We believe that a successful design is one that reflects the
+          individual needs, lifestyle, and preferences of our clients. Eta Group
+          offers a comprehensive portfolio of services for large-scale projects,
+          from luxury residential architecture and refined interior design to
+          modern office spaces and distinctive retail and showroom concepts.
+        </p>
+
+        <h3 className="text-slate-500 text-[1.2rem] font-[600]">
+          Implementation
+        </h3>
+        <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+          Concepts are easy to sketch; executing them is the true Mastery
+        </p>
+        <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+          At Eta Group, we spare clients the stress of finding reliable
+          contractors. We manage the entire process—from initial design to full
+          execution—ensuring every detail aligns perfectly with the original
+          vision.
+        </p>
+
+        <h3 className="text-slate-500 text-[1.2rem] font-[600]">Furniture</h3>
+        <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+          Furniture is the soul of any interior, shaping its mood and character.
+          We provide fully customized furniture for residential, corporate, and
+          specialty spaces, balancing functionality, elegance, and comfort to
+          create interiors that are both refined and inviting.
         </p>
       </section>
     </>
