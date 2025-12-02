01d232ede571388d0b6b2d5bdb9bf38a70387fde
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-02 12:04:10 +0200

    create: paragraphs for founder page

--- app/Founder/page.js
+++ app/Founder/page.js
@@ -9,12 +9,38 @@ import { FaTiktok } from "react-icons/fa6";
 const page = () => {
   return (
     <>
-      <section className="flex items-start m-auto py-[10rem]">
+      <section className="flex items-start gap-[4rem] max-w-[1200px] m-auto py-[10rem]">
         {/* First Column */}
         <section className="flex-1">
           <div>
             <SectionHeader header={"CEO & Founder"} title={"Omar Dora"} />
           </div>
+
+          <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+            Omar Dora is the founder and CEO of Eta Group, leading the company
+            since its establishment in 2014. With a clear vision for high-end
+            interior and architectural design, he has guided Eta Group to become
+            a recognized name in luxury and turnkey solutions across
+            residential, commercial, retail, and hospitality sectors.
+          </p>
+          <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+            Omar emphasizes the balance between creativity, functionality, and
+            client individuality. Under his leadership, Eta Group delivers
+            projects that are both aesthetically exceptional and practical.
+          </p>
+          <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+            Since founding Eta Group, Omar has overseen the completion of
+            hundreds of projects, ensuring consistency, quality, and attention
+            to detail at every stage. His dedication to excellence and
+            innovation has positioned the company as a trusted partner for
+            clients seeking distinctive and refined interior and architectural
+            solutions.
+          </p>
+          <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+            Omar believes successful design comes from close collaboration with
+            clients. He ensures every project reflects the client’s vision while
+            maintaining Eta Group’s high standards.
+          </p>
         </section>
 
         {/* Second Column */}
