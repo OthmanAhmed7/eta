33b9c3a75ebace6f292df3b9fc98550b0d1411cb
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-02 10:59:31 +0200

    fix: spacing in sectionHeader component

--- app/Services/page.js
+++ app/Services/page.js
@@ -10,8 +10,16 @@ const page = () => {
         header={"Explore What We Can Do For You"}
       />
 
-      <section className="flex flex-col m-auto items-start max-w-[1200px]">
+      <section className="flex flex-col m-auto items-start max-w-[1200px] py-[5rem]">
         <SectionHeader header={"Services"} title={"Our Way of Serving You"} />
+
+        <p className="text-slate-500 text-[1.2rem] mb-[2rem]">
+          Every project we craft carries its own signature identity. Eta Group
+          has enhanced hundreds of spaces across residential, commercial,
+          hospitality, and retail sectors, consistently delivering distinguished
+          results. Guided by our core values and an unwavering commitment to
+          quality, we continue to set new standards in unique interior design.
+        </p>
       </section>
     </>
   );
