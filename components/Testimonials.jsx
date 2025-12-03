58cb4788067ed630437e89214d8eb32a67a0cfa6
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-03 13:25:58 +0200

    create: Testimonials data

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Testimonials = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="flex items-center justify-start gap-[5rem] max-w-[1200px] m-auto h-[20rem]">
+        {/* Testimonials Names */}
+        <section className="flex-1"></section>
+
+        {/* Testimonials Content */}
+        <section className="flex-4"></section>
+      </section>
+    </>
+  );
 };
 
 export default Testimonials;
