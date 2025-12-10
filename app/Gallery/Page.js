212bc88fead09f848b878d501a9ef8456e08b517
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-10 07:41:16 +0200

    fix: images doesn't appear in gallery page

--- app/Gallery/Page.js
+++ app/Gallery/Page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { projects } from "@/lib/Projects";
+import Image from "next/image";
 
 const Page = () => {
   return (
@@ -8,13 +9,30 @@ const Page = () => {
         {projects.map((project) => (
           <section key={project.id} id={project.id}>
             <section
-              className="relative h-screen w-full bg-cover bg-no-repeat flex flex-col items-center justify-center px-[1rem]"
-              style={{ backgroundImage: `${project.img}` }}
+              className="relative h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center px-[1rem]"
+              style={{ backgroundImage: `url(${project.img})` }}
             >
-              <p>{project.title}</p>
+              <div className="absolute top-0 left-0 w-full h-full bg-black/45"></div>
+              <p className="text-[4rem] font-[600] text-white z-40 ">
+                {project.title}
+              </p>
             </section>
 
-            <section></section>
+            <section className="py-[5rem]">
+              {project.projects.map((site) => (
+                <section key={site.id} className="grid grid-cols-3">
+                  <div>
+                    <Image
+                      src={site.img}
+                      alt={site.title}
+                      width={120}
+                      height={120}
+                    />
+                    <p>{site.title}</p>
+                  </div>
+                </section>
+              ))}
+            </section>
           </section>
         ))}
       </section>
