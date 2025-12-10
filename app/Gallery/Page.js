4697b3a60ebee51751bf408ff32c2e9d794c0d66
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-10 07:22:50 +0200

    create: project types banner

--- app/Gallery/Page.js
+++ app/Gallery/Page.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { projects } from "@/lib/Projects";
 
 const Page = () => {
-  return <>Gallery</>;
+  return (
+    <>
+      <section>
+        {projects.map((project) => (
+          <section key={project.id} id={project.id}>
+            <section
+              className="relative h-screen w-full bg-cover bg-no-repeat flex flex-col items-center justify-center px-[1rem]"
+              style={{ backgroundImage: `${project.img}` }}
+            >
+              <p>{project.title}</p>
+            </section>
+
+            <section></section>
+          </section>
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default Page;
