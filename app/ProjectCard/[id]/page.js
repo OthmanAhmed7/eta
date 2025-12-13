968542ce29c7d4b859b6855c5d24f3e97ddcc8b4
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-13 09:30:54 +0200

    add: header and images to projects dynamic route

--- app/ProjectCard/[id]/page.js
+++ app/ProjectCard/[id]/page.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { lastWork } from "@/lib/LatestWork";
+import Image from "next/image";
 
-const page = () => {
-  return <>test</>;
+const page = async ({ params }) => {
+  const resolveParams = await params;
+  const projects = lastWork.find((p) => p.id === parseInt(resolveParams.id));
+
+  return (
+    <>
+      <section className="flex items-start gap-[3rem] max-w-[1200px] m-auto py-[5rem] px-[1rem]">
+        <section className="flex-[2]">
+          <h1>{projects.title}</h1>
+        </section>
+
+        <section className="flex-1">
+          {projects.gallery.map((img) => (
+            <div key={img.id}>
+              <Image
+                alt={projects.title}
+                src={img.img}
+                width={1000}
+                height={1000}
+                className="mb-[1rem]"
+              />
+            </div>
+          ))}
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default page;
