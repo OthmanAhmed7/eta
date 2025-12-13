fa18c5c271786384ab82d0703909a9a59ed956aa
Othman Ahmed <othmanahmed258@gmail.com>
2025-12-13 09:42:56 +0200

    add: dynamic routing to Gallery page.js

--- app/Gallery/page.js
+++ app/Gallery/page.js
@@ -26,7 +26,10 @@ const page = () => {
               <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-[2rem] py-[5rem] w-full">
                 {project.projects.map((site) => (
                   <section key={site.id}>
-                    <div className="flex flex-col items-center justify-center gap-[1rem]">
+                    <Link
+                      href={`/ProjectCard/${site.id}`}
+                      className="flex flex-col items-center justify-center gap-[1rem]"
+                    >
                       <div className="relative overflow-hidden group">
                         <Image
                           src={site.img}
@@ -39,7 +42,7 @@ const page = () => {
                         <div className="absolute top-0 left-0 w-full h-full group-hover:bg-black/30 transition duration-500 ease-out"></div>
                       </div>
                       <p className="font-[600] text-[1.25rem]">{site.title}</p>
-                    </div>
+                    </Link>
                   </section>
                 ))}
               </section>
